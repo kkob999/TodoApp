4ac70d7dfba0d3a7877eb9caeaadc3a47cc6bffe
kkob999 <kob.ruji@gmail.com>
2022-11-16 05:42:46 +0700

    Initial commit

---
+++ app/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:app/screens/home.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'TodoApp',
+      home: Home(),
+    );
+  }
+}
